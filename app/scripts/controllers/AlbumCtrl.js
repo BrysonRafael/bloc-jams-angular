bfc391800fc30876d05b2c21eb56b79ad7919f95
Bryson <bryson.rafael@gmail.com>
2016-08-30 17:45:00 -0500

    Checkpoint 6: Completed

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,9 @@
 (function() {
-  function AlbumCtrl() {
-    this.heroTitle = "Turn the Music Up!";
+  function AlbumCtrl(Fixtures) {
+    this.albumData = Fixtures.getAlbum();
   }
 
   angular
     .module('blocJams')
-    .controller('AlbumCtrl', AlbumCtrl);
+    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
